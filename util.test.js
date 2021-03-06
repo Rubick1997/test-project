eedcce3dd6112510e9103aacdb687221cedd688c
Rustam <rustam905@gmail.com>
2021-03-06 15:31:34 -0800

    e2e test with user experience

--- util.test.js
+++ util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require("puppeteer");
 const { generateText, checkAndGenerate } = require("./util");
 
 //unit tests
@@ -19,3 +20,26 @@ test("should generate a valid text output", () => {
 	const text = checkAndGenerate("Rustam", 23);
 	expect(text).toBe("Rustam (23 years old)");
 });
+
+// e2e Tests
+
+test("should create an element with text and correct class", async () => {
+	const browser = await puppeteer.launch({
+		headless: true,
+		slowMo: 80,
+		args: ['--window-size=1920,1080']
+	});
+	const page = await browser.newPage();
+	await page.goto(
+		"file:///Users/rustamkolumbayev/Desktop/My%20Projects/Testing/index.html"
+	);
+	await page.click("input#name");
+	await page.type("input#name", "Rustam");
+	await page.click("input#age");
+	await page.type("input#age", "23");
+	await page.click("#btnAddUser");
+	const finalText = await page.$eval(".user-item", (el) => el.textContent);
+	expect(finalText).toBe("Rustam (23 years old)");
+}, 10000);
+
+
